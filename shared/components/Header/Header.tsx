7d539489571bcd506ad96058f1dd256566c9fcc6
Luan Lorenzato <57531302+LuanMartinsLorenzato@users.noreply.github.com>
2023-04-06 13:58:29 -0300

    Blur in header

--- shared/components/Header/Header.tsx
+++ shared/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import styles from './header.module.scss'
-
+import menuIcon from '../../../public/assets/menu-icon.svg'
+import useWindowDimensions from 'use-window-dimensions';
 const tabs = [
     'Home',
     'Sobre',
@@ -7,16 +8,34 @@ const tabs = [
     'Skills',
     'Contato',
 ]
+const Component: React.FC = () => {
+  const { width, height } = useWindowDimensions();
+
+  console.log("width: " + width + "height: " + height);
+  
+  return (
+    <div>
+      <h1>Height: {width} | Width: {height}</h1>
+    </div>
+  );
+}
 export default function Header () {
+  
   return(
-    <nav className={styles.nav}>
-      {tabs.map(tab =>{
-        return(
-          <button key={tab}>
-            {tab}
-          </button>
-        )
-      })}
-    </nav>
+    <>
+      <header className={styles.header}>
+        <Component />
+        <button>
+          <img src={menuIcon.src} alt="Menu" />
+        </button>
+        {/* {tabs.map(tab =>{
+          return(
+            <button key={tab}>
+              {tab}
+            </button>
+          )
+        })} */}
+      </header>
+    </>
   )
 }
