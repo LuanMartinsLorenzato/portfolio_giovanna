f0c655b910fbcdcd4d450851810c61b387e34a33
Luan Lorenzato <57531302+LuanMartinsLorenzato@users.noreply.github.com>
2023-04-26 14:19:58 -0300

    Header config

--- shared/components/Header/Header.tsx
+++ shared/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import styles from './header.module.scss'
 import menuIcon from '../../../public/assets/menu-icon.svg'
-import useWindowDimensions from 'use-window-dimensions';
+
 const tabs = [
     'Home',
     'Sobre',
@@ -8,23 +8,11 @@ const tabs = [
     'Skills',
     'Contato',
 ]
-const Component: React.FC = () => {
-  const { width, height } = useWindowDimensions();
 
-  console.log("width: " + width + "height: " + height);
-  
-  return (
-    <div>
-      <h1>Height: {width} | Width: {height}</h1>
-    </div>
-  );
-}
 export default function Header () {
-  
   return(
     <>
       <header className={styles.header}>
-        <Component />
         <button>
           <img src={menuIcon.src} alt="Menu" />
         </button>
