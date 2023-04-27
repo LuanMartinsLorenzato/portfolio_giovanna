bf3362a4defda71de60d433be3d36562cbb917d3
Luan Lorenzato <57531302+LuanMartinsLorenzato@users.noreply.github.com>
2023-04-27 01:20:13 -0300

    Header mobile in progress

--- shared/components/Header/Header.tsx
+++ shared/components/Header/Header.tsx
@@ -1,28 +1,62 @@
-import styles from './header.module.scss'
-import menuIcon from '../../../public/assets/menu-icon.svg'
+import styles from './header.module.scss';
+import menuIcon from '../../../public/assets/menu-icon.svg';
+import useWindowResize from '../../hooks/useWindowResize'
+import Link from 'next/link';
+import { useState } from 'react';
 
 const tabs = [
-    'Home',
-    'Sobre',
-    'Projetos',
-    'Skills',
-    'Contato',
+  'Home',
+  'Sobre',
+  'Projetos',
+  'Skills',
+  'Contato',
 ]
 
-export default function Header () {
+export default function Header (): JSX.Element {
+  let isMobile = useWindowResize().width < 500 ? true : false;
+  let [showMenuMobile, setShowMenuMobile] = useState(false);
+
+  const handleMenu = () => {
+    setShowMenuMobile(!showMenuMobile);
+  }
+  
   return(
     <>
-      <header className={styles.header}>
-        <button>
-          <img src={menuIcon.src} alt="Menu" />
-        </button>
-        {/* {tabs.map(tab =>{
-          return(
-            <button key={tab}>
-              {tab}
+      <header className={styles.header} onClick={isMobile ? handleMenu : undefined}>
+        {
+          isMobile ? (
+            <button onClick={handleMenu} className={styles.menuBtn}>
+              <img src={menuIcon.src} alt="Menu" />
             </button>
+          ) :
+          (
+            <nav>
+              {
+                tabs.map(tab => (
+                  <button key={tab} className={styles.tabs}>
+                    {tab}
+                  </button>
+                ))
+              }
+            </nav>
           )
-        })} */}
+        }
+        {showMenuMobile && (
+          <>
+            <div className={styles.overlay}>
+              <nav>
+                {
+                  tabs.map(tab => (
+                    <button key={tab} className={styles.tabs}>
+                      {tab}
+                    </button>
+                  ))
+                }
+              </nav>
+            </div>
+          </>
+          )
+        }
       </header>
     </>
   )
