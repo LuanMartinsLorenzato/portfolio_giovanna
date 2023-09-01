990d6a4f1801cf4b03adcad8c3b0b66d631eb6fb
LuanLorenzato <luan.lorenzato22@outlook.com>
2023-09-01 17:50:09 -0300

    Finish home module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import styles from '../../styles/onePage.module.scss'
 import Header from '../../shared/components/Header/Header';
+import Home from 'modules/Home/Home';
+import bg from '../../public/assets/gif/bgImage.gif'
 
 export default function OnePage() {
   return (
     <>
-      <main className={styles.main}>
+      <main className={styles.main}
+      style={{
+        backgroundImage: `url('${bg.src}')`,
+      }}>
         <Header />
-  {/*   <Home />
-        <About />
+        <Home />
+     {/*<About />
         <Projects />
         <Skills />
-        <Contact /> */}
+        <Contact />*/} 
       </main>
     </>
   )
