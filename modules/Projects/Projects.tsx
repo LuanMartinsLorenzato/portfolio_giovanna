c5886fb591db5ae453a5a227e8a9a369afd74a60
LuanLorenzato <luan.lorenzato22@outlook.com>
2023-09-14 19:55:12 -0300

    Finish module projects

--- modules/Projects/Projects.tsx
+++ modules/Projects/Projects.tsx
@@ -5,6 +5,7 @@ import ProjectsService from 'shared/services/projects-service';
 import { ProjectsType } from 'shared/utils/types';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCards, Pagination } from 'swiper/modules';
+import link_icon from '../../public/assets/link_icon.svg'
 
 import 'swiper/css';
 import 'swiper/css/effect-cards';
@@ -42,9 +43,9 @@ const Projects: React.FC = () => {
 			<div className={styles.projects_wrap}>
 				<div className={styles.project}>
 					<div className={styles.header_description}>
-						<h3>{currentProject?.title}</h3>
+						<h3>{currentProject?.title}.</h3>
 						<div className={styles.line} />
-						<img src="icon" alt="Link Icon" />
+						<img src={link_icon.src} alt="Link Icon" />
 					</div>
 					<div className={styles.description}>
 						<div className={styles.border_line} />
@@ -63,6 +64,7 @@ const Projects: React.FC = () => {
 				>
 					{projects?.map((project, i) => (
 						<SwiperSlide key={i}>
+							<div className={styles.overlay} />
 							<img src={project.thumb} alt="Image thumb" className={styles.thumb} onClick={() => console.log('função')}/>
 						</SwiperSlide>
 					))}
