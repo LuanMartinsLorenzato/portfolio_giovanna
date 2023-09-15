1e23c3bbc977428751d74f3b9a4933c574d7f2e1
LuanLorenzato <luan.lorenzato22@outlook.com>
2023-09-14 21:19:29 -0300

    Add mensage if not image

--- shared/components/modalImages/ModalImages.tsx
+++ shared/components/modalImages/ModalImages.tsx
@@ -2,7 +2,7 @@ import { unmountComponentAtNode } from 'react-dom';
 import styles from './modalImages.module.scss';
 import React from 'react';
 interface ModalInterface {
-  images: Array<string>;
+  images: Array<string> | undefined;
   closeModal: () => void;
 }
 const ModalImages: React.FC<ModalInterface> = ({images, closeModal}) => {
@@ -10,9 +10,14 @@ const ModalImages: React.FC<ModalInterface> = ({images, closeModal}) => {
   return (
     <div className={styles.container_modal}>
       <button onClick={() => closeModal()} />
-      {images.map((url, i) => (
-        <img src={url} alt="Images" key={i}/>
+      {images?.map((url, i) => (
+        <img src={url} alt="Images" key={i} loading='lazy' />
       ))}
+      {!images ? (
+        <>
+          <div className={styles.error_notImages}>Não há imagens neste projeto</div>
+        </>
+      ) : null}
     </div>
   );
 };
