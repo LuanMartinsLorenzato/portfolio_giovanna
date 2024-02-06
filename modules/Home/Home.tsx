2bf1957116c673cf6b35575142108fc332b2b74c
Luan Lorenzato <57531302+LuanMartinsLorenzato@users.noreply.github.com>
2024-02-06 12:02:00 -0300

    Add darkMode in home

--- modules/Home/Home.tsx
+++ modules/Home/Home.tsx
@@ -3,13 +3,16 @@ import styles from './home.module.scss'
 import imgGi from '../../public/assets/imgs/img_gi.png'
 import { useTranslations } from "next-intl";
 import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 const Home: React.FC = () => {
 	const _t = useTranslations("Home");
+	const { theme, setTheme } = useTheme();
+
 
 	return (
 		<section className={styles.container} id='home'>
-			<div className={styles.content_description}>
+			<div className={theme == 'dark' ? styles.content_description_darkMode : styles.content_description}>
 				<h1>{_t('hello')}</h1>
 				<span>{_t('name')}</span>
 				<p>{_t('i')} <span>{_t('design')}</span></p>
