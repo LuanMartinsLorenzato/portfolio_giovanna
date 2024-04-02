861a22f3b12a10e398acc97833d157605595f292
Luan Lorenzato <57531302+LuanMartinsLorenzato@users.noreply.github.com>
2024-04-01 21:12:12 -0300

    Fix header logo animated

--- shared/components/Header/Header.tsx
+++ shared/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import menuIcon_darkMode from "../../../public/assets/menu-icon_darkMode.svg";
 import closeMenuIcon_darkMode from "../../../public/assets/close-menu-icon-dark.svg";
 import closeMenuIcon from "../../../public/assets/close-menu-icon-light.svg";
 import nameIcon from "../../../public/assets/name-icon.svg";
+import nameIconHover from "../../../public/assets/name-icon-hover.svg";
 import iconL_darkMode from "../../../public/assets/iconL_darkMode.svg";
 import iconL from "../../../public/assets/iconL.svg";
 import dayIcon from "../../../public/assets/day-icon.svg";
@@ -11,6 +12,7 @@ import nightIcon from "../../../public/assets/night-icon.svg";
 import useWindowResize from "../../hooks/useWindowResize";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import Image from "next/image";
 import { tabs } from "shared/utils/contants";
 import { useState } from "react";
 import { useRouter } from "next/router";
@@ -20,11 +22,13 @@ export default function Header(): JSX.Element {
   let isMobile = useWindowResize().width < 1200 ? true : false;
   let [showMenuMobile, setShowMenuMobile] = useState(false);
   let [isActiveLanguage, setIsActiveLanguage] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
   const _t = useTranslations("Header");
   const { route } = useRouter();
   const { theme, setTheme } = useTheme();
-  let [iconColors, setIconColors] = useState(theme == 'dark' ? nightIcon.src : dayIcon.src);
-
+  let [iconColors, setIconColors] = useState(
+    theme == "dark" ? nightIcon.src : dayIcon.src
+  );
 
   const handleMenu = () => {
     setShowMenuMobile(!showMenuMobile);
@@ -39,10 +43,8 @@ export default function Header(): JSX.Element {
   };
 
   const handleDarkMode = () => {
-    setTheme(theme == 'dark' ? 'light' : 'dark')
-    theme == 'dark' 
-      ? setIconColors(dayIcon.src)
-      : setIconColors(nightIcon.src);
+    setTheme(theme == "dark" ? "light" : "dark");
+    theme == "dark" ? setIconColors(dayIcon.src) : setIconColors(nightIcon.src);
   };
 
   return (
@@ -55,11 +57,13 @@ export default function Header(): JSX.Element {
           <>
             <div className={styles.actions}>
               <button onClick={handleLanguage}>
-                <img
-                  src={theme == 'dark' ? iconL_darkMode.src : iconL.src}
+                <Image
+                  src={theme == "dark" ? iconL_darkMode.src : iconL.src}
                   alt="Languages"
                   width={25}
                   height={25}
+                  priority={true}
+                  placeholder="empty"
                 />
               </button>
               <div
@@ -67,18 +71,34 @@ export default function Header(): JSX.Element {
                 id="slideOff"
               >
                 <button>
-                  <Link href={route} locale={"en"} className={theme == 'dark' ? styles.language_link_darkMode : styles.language_link}>
+                  <Link
+                    href={route}
+                    locale={"en"}
+                    className={
+                      theme == "dark"
+                        ? styles.language_link_darkMode
+                        : styles.language_link
+                    }
+                  >
                     <span>English</span>
                   </Link>
                 </button>
                 <button>
-                  <Link href={route} locale={"pt"} className={theme == 'dark' ? styles.language_link_darkMode : styles.language_link}>
+                  <Link
+                    href={route}
+                    locale={"pt"}
+                    className={
+                      theme == "dark"
+                        ? styles.language_link_darkMode
+                        : styles.language_link
+                    }
+                  >
                     <span>Português - Br</span>
                   </Link>
                 </button>
               </div>
               <button onClick={handleDarkMode}>
-                <img
+                <Image
                   src={iconColors}
                   alt="Change colors"
                   width={25}
@@ -94,23 +114,52 @@ export default function Header(): JSX.Element {
                   : `${styles.blur_menu} ${styles.menuBtn}`
               }
             >
-              <img src={theme == 'dark' ? menuIcon_darkMode.src : menuIcon.src} alt="Menu" />
+              <Image
+                src={theme == "dark" ? menuIcon_darkMode : menuIcon}
+                alt="Menu"
+              />
             </button>
           </>
         ) : (
           <>
             <nav className={styles.navBar_desktop}>
-              <img src={nameIcon.src} alt="Menu" className={styles.name_icon} />
+              <div
+                className={styles.container_imgs}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              >
+                <div className={`${styles.image_content} ${isHovered ? styles.fade_out : styles.fade_in}`}>
+                  <Image
+                    src={nameIcon}
+                    alt="Owner logo"
+                    priority={true}
+                  />
+                </div>
+                <div className={`${styles.image_content} ${!isHovered ? styles.fade_out : styles.fade_in}`}>
+                  <Image
+                    src={nameIconHover}
+                    alt="Owner logo"
+                    priority={true}
+                  />
+                </div>
+              </div>
+
               {tabs.map((tab) => (
-                <a href={tab.path} key={tab.name} className={theme === 'dark' ? styles.tabs_darkMode : styles.tabs}>
+                <a
+                  href={tab.path}
+                  key={tab.name}
+                  className={
+                    theme === "dark" ? styles.tabs_darkMode : styles.tabs
+                  }
+                >
                   {_t(tab.name)}
                 </a>
               ))}
             </nav>
             <div className={styles.actions}>
               <button onClick={handleLanguage}>
-                <img
-                  src={theme == 'dark' ? iconL_darkMode.src : iconL.src}
+                <Image
+                  src={theme == "dark" ? iconL_darkMode.src : iconL.src}
                   alt="Languages"
                   width={30}
                   height={30}
@@ -121,18 +170,34 @@ export default function Header(): JSX.Element {
                 id="slideOff"
               >
                 <button>
-                  <Link href={route} locale={"en"} className={theme == 'dark' ? styles.language_link_darkMode : styles.language_link}>
+                  <Link
+                    href={route}
+                    locale={"en"}
+                    className={
+                      theme == "dark"
+                        ? styles.language_link_darkMode
+                        : styles.language_link
+                    }
+                  >
                     <span>English</span>
                   </Link>
                 </button>
                 <button>
-                  <Link href={route} locale={"pt"} className={theme == 'dark' ? styles.language_link_darkMode : styles.language_link}>
+                  <Link
+                    href={route}
+                    locale={"pt"}
+                    className={
+                      theme == "dark"
+                        ? styles.language_link_darkMode
+                        : styles.language_link
+                    }
+                  >
                     <span>Português - Br</span>
                   </Link>
                 </button>
               </div>
               <button onClick={handleDarkMode}>
-                <img
+                <Image
                   src={iconColors}
                   alt="Change colors"
                   width={35}
@@ -144,23 +209,44 @@ export default function Header(): JSX.Element {
         )}
         {showMenuMobile && (
           <>
-            <div className={theme == 'dark' ? styles.overlay_darkMode : styles.overlay}>
-              <nav className={theme == 'dark' ? styles.navBar_darkMode : styles.navBar}>
+            <div
+              className={
+                theme == "dark" ? styles.overlay_darkMode : styles.overlay
+              }
+            >
+              <nav
+                className={
+                  theme == "dark" ? styles.navBar_darkMode : styles.navBar
+                }
+              >
                 <div className={styles.border_position}>
                   <div className={styles.border_gradient} />
                   <div className={styles.border_gradient} />
                 </div>
-                <img
+                <Image
                   src={nameIcon.src}
                   alt="Menu"
                   className={styles.name_icon}
                 />
                 <button onClick={handleMenu} className={styles.closeMenuBtn}>
-                  <img src={theme == 'dark' ? closeMenuIcon_darkMode.src : closeMenuIcon.src} alt="Menu" />
+                  <Image
+                    src={
+                      theme == "dark"
+                        ? closeMenuIcon_darkMode.src
+                        : closeMenuIcon.src
+                    }
+                    alt="Menu"
+                  />
                 </button>
                 <div className={styles.container_tabs}>
                   {tabs.map((tab) => (
-                    <a href={tab.path} key={tab.name} className={theme === 'dark' ? styles.tabs_darkMode : styles.tabs}>
+                    <a
+                      href={tab.path}
+                      key={tab.name}
+                      className={
+                        theme === "dark" ? styles.tabs_darkMode : styles.tabs
+                      }
+                    >
                       {_t(tab.name)}
                     </a>
                   ))}
